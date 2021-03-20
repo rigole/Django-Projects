0c135c5d87469e36008e0d76715492861034d9ee
rigole <foplacide@gmail.com>
2021-03-20 13:41:31 +0100

    creating a new view

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World")
+
+def item (request):
+    return HttpResponse('This s an Item view')
