e28c8df198b11b492a8c0d8ea143025b2512c445
rigole <foplacide@gmail.com>
2021-03-22 22:12:09 +0100

    creating a my own view + html

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('item', views.item, name = 'item'),
+    path('goal',views.goal, name='goal')
 ]
--- mysite/food/views.py
+++ mysite/food/views.py
@@ -6,3 +6,5 @@ def index(request):
 
 def item (request):
     return HttpResponse('This s an Item view')
+def goal (request):
+    return HttpResponse('<h1>I am on the way to success and I will be a Billionaire</h1>')
