487c4b587e42e353bb318d36975e8e1b1daf30e5
rigole <foplacide@gmail.com>
2021-03-23 09:18:48 +0100

    reading data from database

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    item_list = Item.objects.all()
+    return HttpResponse(item_list)
 
 def item (request):
     return HttpResponse('This s an Item view')
+
 def goal (request):
     return HttpResponse('<h1>I am on the way to success and I will be a Billionaire</h1>')
+
+
