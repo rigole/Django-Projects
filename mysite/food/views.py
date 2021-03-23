6b50087b8b02620bfbab1696c9eaec4d44342605
rigole <foplacide@gmail.com>
2021-03-23 09:32:41 +0100

    creating and rendering my first django template

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Item
+from django.template import  loader
 # Create your views here.
 def index(request):
     item_list = Item.objects.all()
-    return HttpResponse(item_list)
+    template = loader.get_template('food/index.html')
+    context= {
+
+    }
+    return HttpResponse(template.render(context,request))
 
 def item (request):
     return HttpResponse('This s an Item view')
