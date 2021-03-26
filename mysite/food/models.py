7aade6b83abac0a68bd60229760cbe4a0ace4d93
rigole <foplacide@gmail.com>
2021-03-26 15:56:04 +0100

    Adding inage field in database and in static files

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -8,3 +8,4 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500, default="https://s3.amazonaws.com/ODNUploads/532dfdbcc6479placeholder_food_item_2.png")
