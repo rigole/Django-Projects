2fbc6f6e0ef252e3009cfe26d036bfc5e476fa32
rigole <foplacide@gmail.com>
2021-03-28 22:13:52 +0100

    creating the register view

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'users.apps.UsersConfig'
+    'users.apps.UsersConfig',
     'food.apps.FoodConfig',
     'django.contrib.admin',
     'django.contrib.auth',
