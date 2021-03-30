aec30ff873a243ffdeba5e5ce22393c7f0256e8e
rigole <foplacide@gmail.com>
2021-03-30 23:12:29 +0100

    creating profile model

--- mysite/users/models.py
+++ mysite/users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import  User
 # Create your models here.
+class Profile(models.Model):
+     user = models.OneToOneField(User,on_delete=models.CASCADE)
+     image = models.ImageField(default='profile.jpg', upload_to='profile_pictures')
+     location = models.CharField(max_length=100)
