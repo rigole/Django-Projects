f2605c8d7ec68993c91322e5af9dfc936b9ef4e5
rigole <foplacide@gmail.com>
2021-04-02 13:19:05 +0100

    adding absolute url for default page after signing up

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 # Create your models here.
 class Item(models.Model):
 
@@ -10,3 +11,7 @@ class Item(models.Model):
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
     item_image = models.CharField(max_length=500, default="https://s3.amazonaws.com/ODNUploads/532dfdbcc6479placeholder_food_item_2.png")
+
+
+    def get_absolute_url(self):
+        return reverse("food:detail", kwargs={"pk": self.pk})
