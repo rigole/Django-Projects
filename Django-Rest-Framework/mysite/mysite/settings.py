28c72d2e4f822fe3d5956bba8f899ec624d74391
rigole <foplacide@gmail.com>
2021-04-04 01:34:30 +0100

    setting up the new projects

--- Django-Rest-Framework/mysite/mysite/settings.py
+++ Django-Rest-Framework/mysite/mysite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'movies.apps.MoviesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
