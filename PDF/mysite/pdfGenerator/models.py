d4ce840bad85267b82e3977131aae42e759251f0
rigole <foplacide@gmail.com>
2021-08-24 16:59:38 +0100

    creating the form to let user enter his information

--- PDF/mysite/pdfGenerator/models.py
+++ PDF/mysite/pdfGenerator/models.py
@@ -10,6 +10,6 @@ class Profile(models.Model):
     degree = models.CharField(max_length=200)
     school = models.CharField(max_length=200)
     university = models.CharField(max_length=200)
-    previous_work = models.CharField(max_length=1000)
+    previous_work = models.TextField(max_length=1000)
     skills = models.TextField(max_length=1000)
 
