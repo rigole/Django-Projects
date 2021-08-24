26af559e5fb44dee677509a7e3e51c06ef724cc9
rigole <foplacide@gmail.com>
2021-08-24 17:55:15 +0100

    saving the object in database

--- PDF/mysite/pdfGenerator/views.py
+++ PDF/mysite/pdfGenerator/views.py
@@ -3,4 +3,17 @@ from .models import Profile
 # Create your views here.
 
 def accept(request):
+    if request.method == "POST":
+        name = request.POST.get("name","")
+        email = request.POST.get("email", "")
+        phone = request.POST.get("phone", "")
+        summary = request.POST.get("summary", "")
+        degree = request.POST.get("degree", "")
+        university = request.POST.get("university", "")
+        school = request.POST.get("school", "")
+        previous_work = request.POST.get("previous_work", "")
+        skills = request.POST.get("skills", "")
+
+        profile = Profile(name=name, email=email,phone=phone,summary=summary,degree=degree,university=university,school=school,previous_work=previous_work,skills=skills)
+        profile.save()
     return render(request, 'pdf/form.html')
