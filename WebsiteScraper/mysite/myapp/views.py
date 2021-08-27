a852ecfda1c97515bc4d9f5f1ac06e8a97a08490
rigole <foplacide@gmail.com>
2021-08-27 21:14:56 +0100

    rendering all the link and link's name of the site google.com and save them in database

--- WebsiteScraper/mysite/myapp/views.py
+++ WebsiteScraper/mysite/myapp/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
-
+from .models import Link
 # Create your views here.
 
 def scrape(request):
     page = requests.get('https://www.google.com')
     soup = BeautifulSoup(page.text, 'html.parser')
 
-    link_adress = []
 
     for link in soup.find_all('a'):
-        link_adress.append(link.get('href'))
+        link_address = link.get('href')
+        link_text = link.string
+        Link.objects.create(address=link_address, name=link_text)
 
-    return render(request, 'myapp/result.html', {'link_address': link_adress})
+    data = Link.objects.all()
+
+    return render(request, 'myapp/result.html', {'data': data})
