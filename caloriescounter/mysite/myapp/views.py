776a91584993ab7bb5d0b6ec4a3aaab6ae41f5a6
rigole <foplacide@gmail.com>
2021-09-04 22:12:33 +0100

    adding food through form

--- caloriescounter/mysite/myapp/views.py
+++ caloriescounter/mysite/myapp/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from .models import Food
+from .models import Food, Consume
 # Create your views here.
 
 def index(request):
-    foods = Food.objects.all()
+
+    if request.method == "POST":
+        food_consumed = request.POST['food_consumed']
+        consume = Food.objects.get(name=food_consumed)
+        user = request.user
+        consume = Consume(user=user,food_consumed=consume)
+        consume.save()
+        foods = Food.objects.all()
+    else:
+        foods = Food.objects.all()
     return render(request, 'myapp/index.html',{'foods':foods})
